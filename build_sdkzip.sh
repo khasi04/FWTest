ee4fb986a3c0b47d0c214b906723f2cc3fc95d68
Koichi Hashimoto <hashimoto@unerry.co.jp>
2021-03-24 12:14:59 +0900

    ディレクトリ構成変更

--- build_sdkzip.sh
+++ build_sdkzip.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
 set -x
 
 SDK_VERSION=$1
 
-SDK_ROOT_DIR=release/download
+SDK_ROOT_DIR=releases/download
 CONFIGURATION_BUILD_DIR=build
 
 create_sdk () {
 	SDK_NAME=$1
 	CONF=$2
-	SDK_DIR=$SDK_ROOT_DIR/$SDK_NAME
+	SDK_DIR=$SDK_ROOT_DIR/${SDK_VERSION}/$SDK_NAME
 	mkdir -p $SDK_DIR
 
 	cp -rf "${CONFIGURATION_BUILD_DIR}/${CONF}-iphoneos/FWTest.framework" $SDK_DIR
 
-  (cd $SDK_ROOT_DIR && zip -r $SDK_NAME.zip $SDK_NAME)
+  (cd $SDK_ROOT_DIR//${SDK_VERSION} && zip -r $SDK_NAME.zip $SDK_NAME)
 }
 
 create_sdk FWTest "Release"
